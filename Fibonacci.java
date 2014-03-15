883c5777d55daca0444d06fa1448eaf703dde8fa
comtin <comtin@gmail.com>
2014-03-15 16:43:57 +0900

    implement negative fibonacci.

--- Fibonacci.java
+++ Fibonacci.java
@@ -4,7 +4,7 @@ import java.math.BigInteger;
  * 피보나치 수열을 구하는 클래스입니다.
  * 피보나치 수열이 뭔가요? http://ko.wikipedia.org/wiki/%ED%94%BC%EB%B3%B4%EB%82%98%EC%B9%98_%EC%88%98
  */
-public class Fibonacci{
+public class Fibonacci {
      public static void main(String []args){
          if (args.length == 0) {
              print_usage(Fibonacci.class.getName());
@@ -12,12 +12,24 @@ public class Fibonacci{
          }
          int start = Integer.parseInt(args[0]);
          BigInteger fib1 = new BigInteger("0");
-         BigInteger fib2 = new BigInteger("1");
-         for(int i = 0; i <= (start -2); i++) {
-             BigInteger fib3 = fib1.add(fib2);
-             System.out.println("F"+ i + " = " + fib1 + ", F" + (i+1) + " = " + fib2 + ", F" + (i+2) + " =  " + fib3);
-             fib1 = fib2;
-             fib2 = fib3;
+         if (start >= 0) {
+             BigInteger fib2 = new BigInteger("1");
+             for(int i = 0; i <= (start-2); i++) {
+                 BigInteger fib3 = fib1.add(fib2);
+                 System.out.println("F"+ i + " = " + fib1 + ", F" + (i+1) + " = " + fib2 + ", F" + (i+2) + " =  " + fib3);
+                 fib1 = fib2;
+                 fib2 = fib3;
+             }
+         } else {
+             // negative fibonacci implements  #52
+             BigInteger fib2 = new BigInteger("-1");
+
+             for (int i = 0; i > (start+1); i--) {
+                 BigInteger fib3 = fib1.add(fib2);
+                 System.out.println("F"+ i + " = " + fib1 + ", F" + (i-1) + " = " + fib2 + ", F" + (i-2) + " =  " + fib3);
+                 fib1 = fib2;
+                 fib2 = fib3;
+             }
          }
      }
      public static void print_usage(String progname) {
